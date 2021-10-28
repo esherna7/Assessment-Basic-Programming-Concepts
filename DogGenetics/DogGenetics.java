30aa76bc087622707e1dadf54dc2ebea308e444d
Esai Hernandez <hernandezesai123@gmail.com>
2021-10-28 12:57:03 -0500

    Added comments to DogGenetics.java

--- DogGenetics/DogGenetics.java
+++ DogGenetics/DogGenetics.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// author Esai Hernandez
 
-/**
- *
- * @author Esai Hernandez
- */
+// Write a program that asks the user for the name of their dog, and then generates a fake DNA background report on the pet dog.
+// It should assign a random percentage to 5 dog breeds (that should add up to 100%!) 
+
 import java.util.*;
 
 public class DogGenetics {
@@ -35,6 +30,7 @@ public class DogGenetics {
         input.close();
     }
 
+    // generates 5 random percentages that add up to 100
     public static void generateRandomPercentages() {
         Random random = new Random();
 
