83c9998616f5f98028e2cf5ea2d2d0f3c9d7997c
Esai Hernandez <hernandezesai123@gmail.com>
2021-10-28 12:54:33 -0500

    Added comments to HealthyHearts.java

--- HealthyHearts/HealthyHearts.java
+++ HealthyHearts/HealthyHearts.java
@@ -1,3 +1,8 @@
+// author Esai Hernandez
+// Make a simple calculator that asks the user for their age. Then calculate the healthy heart rate range for that age, and display it.
+// Their maximum heart rate should be 220 - their age.
+// The target heart rate zone is the 50 - 85% of the maximum.
+
 import java.util.*;
 
 public class HealthyHearts{
@@ -6,10 +11,12 @@ public class HealthyHearts{
         Scanner input = new Scanner(System.in);
         int maxHeartRate = 220;
 
+        // Read user age input
         System.out.print("What is your age? ");
         int inputAge = Integer.parseInt(input.nextLine());
         maxHeartRate -= inputAge;
 
+        // calculate and print max heart rate and target HR zone
         System.out.println("Your maximum heart rate should be " + maxHeartRate + " beats per minute");
         System.out.println("Your target HR Zone is " + (int)(Math.round(maxHeartRate * .50)) + " - " + (int)(Math.round(maxHeartRate * .85)) + " beats per minute");
 
